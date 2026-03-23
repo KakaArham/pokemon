64bbef9e242707c4ffa564c25aba9032ce151e7a
kaka <depanajaaaa@gmail.com>
2026-03-23 16:38:28 +0700

    Initial commit: Web Pokemon siap beraksi

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/pokemon/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
